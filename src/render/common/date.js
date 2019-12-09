916fecd8fd81bb77d423801e28a2a7da40d3f28e
ehuanrum <huanrum@126.com>
2019-12-09 21:34:05 +0800

    add month select

--- src/render/common/date.js
+++ src/render/common/date.js
@@ -11,14 +11,24 @@ class MDatePicker extends Component {
     this.refs.panel.querySelector('input').focus();
   };
   render(){
+    const {mode, ...props} = this.props;
+    let Wapper = DatePicker;
+    if(mode === 'ym'){
+      Wapper = DatePicker.MonthPicker;
+    } else if(mode === 'range'){
+      Wapper = DatePicker.RangePicker;
+    }
     return (
       <span ref="panel">
-        <DatePicker {...this.props} onChange={this.handleChange}/>
+        <Wapper {...props} onChange={this.handleChange}/>
       </span>
     )
   }
 }
 
+/**
+ * mode 可选值 为ym月份选择器，range范围选择器
+ */
 export function date({onChange, onBlur, ...props}){
   const handleChange = (e,value) => {
     onChange(e,value).then(()=>onBlur())
