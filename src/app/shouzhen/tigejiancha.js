6ae68d1360dcfb9d4056aff7f5150cc65fc092e9
kisixing <kisixing@foxmail.com>
2019-12-24 12:22:55 +0800

    checkup commit

--- src/app/shouzhen/tigejiancha.js
+++ src/app/shouzhen/tigejiancha.js
@@ -16,10 +16,10 @@ export default class extends Component{
       rows: [
         {
           columns:[
-            {name:'ckcurtizh(kg)[现体重]', type:'input', span:6, valid: 'required|number|rang(0,500)'},
+            {name:'cktizh(kg)[现体重]', type:'input', span:6, valid: 'required|number|rang(0,500)'},
             {span:1},
             { 
-              name: 'ckshrinkpressure(mmHg)[血压]', type: ['input(/)','input'], span: 6, valid: (value)=>{
+              name: 'ckpressure(mmHg)[血压]', type: ['input(/)','input'], span: 6, valid: (value)=>{
               let message = '';
               if(value){
                 message = [0,1].map(i=>valid(`number|required|rang(0,${[139,109][i]})`,value[i])).filter(i=>i).join();
@@ -29,12 +29,12 @@ export default class extends Component{
               return message;
             }},
             {span:2},
-            {name:'mb(次/分)[脉搏]', type:'input', span:6, valid: 'required|number|rang(0,100)'},
+            {name:'add_FIELD_pulse(次/分)[脉搏]', type:'input', span:6, valid: 'required|number|rang(0,100)'},
           ]
         },
         {
           columns:[
-            {name:'yqtz(kg)[孕前体重]', type:'input', span:6, valid: 'required|number|rang(10,100)'},
+            {name:'ckcurtizh(kg)[孕前体重]', type:'input', span:6, valid: 'required|number|rang(10,100)'},
             {span:1},
             {name:'cksheng(cm)[身高]', type:'input', span:6, valid: 'required|number|rang(150,250)'},
             {span:2},
@@ -43,51 +43,51 @@ export default class extends Component{
         },
         {
           columns:[
-            {name:'pflm[皮肤黏膜]', className:'col-94-sp', type:'checkinput-6',valid: 'required', options: baseData.pfOptions,span:13}
+            {name:'ckpifu[皮肤黏膜]', className:'col-94-sp', type:'checkinput-6',valid: 'required', options: baseData.pfOptions,span:13}
           ]
         },
         {
           columns:[
-            {name:'jzx[甲状腺]', type:'checkinput-3', valid: 'required', options: baseData.neOptions,radio:true,span:7},
-            {name:'xgzy[血管杂音]', type:'checkinput-3',valid: 'required', options: baseData.hnOptions,radio:true,span:7},
+            {name:'ckjiazhx[甲状腺]', type:'checkinput-3', valid: 'required', options: baseData.neOptions,radio:true,span:7},
+            {name:'vascularMurmur[血管杂音]', type:'checkinput-3',valid: 'required', options: baseData.hnOptions,radio:true,span:7},
             {span:1},
-            {name:'id3[其他]', type:'input', span:6},
+            {name:'vascularMurmurOther[其他]', type:'input', span:6},
           ]
         },
         {
           columns:[
-            {name:'rt[乳头]', type:'checkinput-3',valid: 'required', options: baseData.rtOptions,radio:true,span:7}
+            {name:'ckrut[乳头]', type:'checkinput-3',valid: 'required', options: baseData.rtOptions,radio:true,span:7}
           ]
         },
         {
           columns:[
-            {name:'hxy[呼吸音]', type:'checkinput-3',valid: 'required', options: baseData.neOptions,radio:true,span:7},
-            {name:'ly[啰音]', type:'input', span:6,valid: 'required'},
+            {name:'breathSounds[呼吸音]', type:'checkinput-3',valid: 'required', options: baseData.neOptions,radio:true,span:7},
+            {name:'breathSoundsOther[啰音]', type:'input', span:6,valid: 'required'},
           ]
         },
         {
           columns:[
-            {name:'xl(次/分)[心率]', type:'input',valid: 'required', span:6},
+            {name:'cardiac(次/分)[心率]', type:'input',valid: 'required', span:6},
             {span:1},
-            {name:'xl2[心律]', type:'checkinput-3',valid: 'required', options: baseData.xlOptions,radio:true,span:7},
+            {name:'heart[心律]', type:'checkinput-3',valid: 'required', options: baseData.xlOptions,radio:true,span:7},
             {span:1},
-            {name:'xlzy[杂音]', type:'checkinput-3',valid: 'required', options: baseData.hnOptions,radio:true,span:9},
+            {name:'murmurs[杂音]', type:'checkinput-3',valid: 'required', options: baseData.hnOptions,radio:true,span:9},
           ]
         },
         {
           columns:[
-            {name:'gan[肝]', type:'checkinput-3',valid: 'required', options: baseData.cjOptions,radio:true,span:7},
-            {name:'pi[脾]', type:'checkinput-3',valid: 'required', options: baseData.cjOptions,radio:true,span:7},
+            {name:'ckganz[肝]', type:'checkinput-3',valid: 'required', options: baseData.cjOptions,radio:true,span:7},
+            {name:'ckpiz[脾]', type:'checkinput-3',valid: 'required', options: baseData.cjOptions,radio:true,span:7},
             {span:1},
-            {name:'sqt[肾区叩痛]', type:'checkinput-3',valid: 'required', options: baseData.sktOptions,span:9},
+            {name:'ckshenz[肾区叩痛]', type:'checkinput-3',valid: 'required', options: baseData.sktOptions,span:9},
           ]
         },
         {
           columns:[
-            {name:'jz[脊柱]', type:'checkinput-3',valid: 'required', options: baseData.jxOptions,radio:true,span:7},
-            {name:'xzfz[下肢浮肿]', type:'checkinput', className:'col-xz-sp', valid: 'required', options: baseData.xzfOptions,radio:true,span:14},
+            {name:'ckjizh[脊柱]', type:'checkinput-3',valid: 'required', options: baseData.jxOptions,radio:true,span:7},
+            {name:'ckfuzh[下肢浮肿]', type:'checkinput', className:'col-xz-sp', valid: 'required', options: baseData.xzfOptions,radio:true,span:14},
             {span:1},
-            {name:'sqfs[双膝反射]', type:'checkinput', className:'col-97-sp', valid: 'required', options: baseData.sxfOptions,radio:true,span:9},
+            {name:'nervousReflex[双膝反射]', type:'checkinput', className:'col-97-sp', valid: 'required', options: baseData.sxfOptions,radio:true,span:9},
           ]
         },
       ]
