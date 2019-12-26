e5aeee414543acc8c30ae9d4798224fce95d1b14
Dukee Xiao <xiao8279586@163.com>
2019-12-26 20:48:22 +0800

    fix bug

--- src/app/fuzhen/util.js
+++ src/app/fuzhen/util.js
@@ -15,8 +15,13 @@ export function formateDate() {
 
 export function getWeek(param1, param2) {
   let day1 = param1 * 7;
-  let day2 = param2.split('+');
-  day2 = parseInt(day2[0] * 7) + parseInt(day2[1]);
+  let day2;
+  if (param2.indexOf('+') !== -1) {
+    day2 = param2.split('+');
+    day2 = parseInt(day2[0] * 7) + parseInt(day2[1]);
+  } else {
+    day2 = parseInt(param2) * 7;
+  }
   let days = day1 - day2;
   return `${Math.floor(days / 7)}+${days % 7}`;
 }
